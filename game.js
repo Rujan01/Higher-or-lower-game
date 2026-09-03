cb0f8741d60a946b882c6422a55af134a32ec072
Your Name <you@example.com>
2026-09-03 08:34:32 +0545

    Promise

---
+++ game.js
@@ -0,0 +1,35 @@
+const input = require("readline-sync");
+// taking first player information
+let name1=input.question("enter first player name");
+let num1=Number("enter your number");
+//taking second player information
+let name2=input.question("enter second player name");
+let num2=Number("enter your number");
+while(true){
+//quessing logic
+//player one guessing
+let guess =Number(input.question('player one guess the number'));
+if(guess===num2)
+{
+    console.log("player one is winner");
+    break;
+}
+else if(guess<num2)
+{
+    console.log("hint:Higher");
+}else{
+    console.log("hint:Lower");
+}
+//player two guessing
+guess =Number(input.question('player two guess the number'));
+if(guess===num1)
+{
+    console.log("player two is winner");
+}
+else if(guess<num1)
+{
+    console.log("hint:Higher");
+}else{
+    console.log("hint:Lower");
+}
+}
