e93afbc66c36c4abbf12cda8d6f20144011a872e
Your Name <you@example.com>
2026-09-03 08:54:10 +0545

    add on

--- promise.js
+++ promise.js
@@ -1,13 +1,13 @@
 const checkLogin=new Promise((resolve,reject)=>{
 let loggedIn=true;
 if(loggedIn){
-    resolve("welcome you are logged in");
+    resolve("welcome you are logged in")
 }
 else{
-    reject("sorry you are not logged in");
+    reject("sorry you are not logged in")
 }
 }
 )
 
-checkLogin.then((result)=>{console.log(result)})
-checkLogin.catch((error)=>{console.log(error)})
+checkLogin.then((result)=>(console.log(result)))
+checkLogin.catch((error)=>(console.log(error)))
